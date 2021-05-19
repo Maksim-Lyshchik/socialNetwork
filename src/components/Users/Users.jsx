7440fe49d3459b9d82010b428ea5663936d967bb
Name <email@example.ru>
2021-05-19 23:37:16 +0300

    54_componentDidMount Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,18 +5,16 @@ import userPhoto from '../../assets/images/users.png'
 
 class Users extends React.Component {
 
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    this.props.setUsers(response.data.items);
-                })
-        }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
     }
 
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
+
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>
