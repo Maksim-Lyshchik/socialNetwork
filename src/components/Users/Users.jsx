5b9674d596d74bdcc5b8508dac3100e2f96d63bf
Name <email@example.ru>
2021-05-19 23:00:28 +0300

    53_extends React.Component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,74 +1,53 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from "axios";
+import userPhoto from '../../assets/images/users.png'
 
-let Users = (props) => {
-if(props.users.length ===0) {
-    props.setUsers(
-        [{
-            id: 1,
-            fotoUrl: 'https://odnaminyta.com/wp-content/uploads/2020/11/galkin.jpg',
-            followed: false,
-            fullName: 'Maksim',
-            status: 'hi everione!',
-            location: {city: 'Hrodno', country: 'Belarus'}
-        },
-            {
-                id: 2,
-                fotoUrl: 'https://odnaminyta.com/wp-content/uploads/2020/11/galkin.jpg',
-                followed: true,
-                fullName: 'Nastya',
-                status: 'hi girl!',
-                location: {city: 'Hrodno', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                fotoUrl: 'https://odnaminyta.com/wp-content/uploads/2020/11/galkin.jpg',
-                followed: true,
-                fullName: 'Maksim',
-                status: 'hi gays!',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 4,
-                fotoUrl: 'https://odnaminyta.com/wp-content/uploads/2020/11/galkin.jpg',
-                followed: false,
-                fullName: 'Maksim',
-                status: 'hi frends!',
-                location: {city: 'Warshawa', country: 'Poland'}
-            },]
-    )
-}
+class Users extends React.Component {
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                })
+        }
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
-                    <div><img src={u.fotoUrl} className={styles.userPhoto}/></div>
+                    <div><img src={u.photos.small != null ? u.photos.small : userPhoto}
+                              className={styles.userPhoto}/></div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
+                        <span>
                     <span>
-                    <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
 
 
-                </div>
-            )}
-    </div>
+                    </div>
+                )}
+        </div>
+    }
 }
 
 export default Users;
