37180df21c63e920fd779c3a6d3c2b6f3ad8cfff
Name <email@example.ru>
2021-06-09 22:10:40 +0300

    84-85 Hook useState useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import PreLoader from '../../common/Preloader/Preloader';
 import jobLooking from '../../../assets/images/job.svg';
-import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
         </div>*/}
         <div className={s.discriptionBlock}>
             <img src={props.profile.photos.large}/>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             <div>
                 <h2>{props.profile.fullName}</h2>
                 <span>{props.profile.aboutMe}</span>
